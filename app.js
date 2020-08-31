a675fbc863f4ed14ea48038b3a7aae986b6c4c58
Saumya Kushwaha <saumyakushwaha14@gmail.com>
2020-09-01 00:11:53 +0530

    Second commit.

--- app.js
+++ app.js
@@ -14,42 +14,47 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(path.join(__dirname,"public")))
 
+let ytData=" "
+
+
+
 app.post("/", function(req, res) {
   const query = req.body.title
-  const apikey = "AIzaSyDMLC8_loigs-wt1Tco4QkBu4iWRm5TKFo"
+  const apikey = "AIzaSyA4tF7X8Fqkhdoe7xl5LyJtNIZPUYJZLIE"
   const url = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + apikey + "&type=video&q=" + query + "&maxResults=2"
 
+
   https.get(url, (resp) => {
-    let data = '';
+  let data = '';
     console.log(resp.statusCode);
 
     // A chunk of data has been recieved.
+
     resp.on('data', (chunk) => {
+
       data += chunk;
     });
 
     resp.on('end', () => {
-      const ytData = JSON.parse(data);
-      console.log(ytData);
+      // console.log(data);
+       ytData=JSON.parse(data)
+       console.log(ytData);
 
-      ytData.items.forEach(item => {
-        
-           video = `
-               <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
-                  `
-            $("#videos").append(video)
-
-       })
+      // ytData.items.forEach(item => {
+      //      video = `
+      //          <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+      //             `
+      //        $("#videos").append(video)
+      //      })
 
       })
+   });
+   })
 
-    })
-
-  });
-
-app.get("/", function(req, res) {
-    res.render("index")
+  app.get("/", function(req, res) {
+    res.render("index",{ytData:ytData})
 })
+
 
   app.listen(3000, function() {
   console.log("Server started on port 3000");
--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 })
 
 function searchVideo(){
-       video = `
-           <iframe width="420" height="315" src="http://www.youtube.com/embed/4F2m91eKmts" frameborder="0" allowfullscreen></iframe>
-              `
-        $("#videos").append(video)
+       // video = `
+       //     <iframe width="420" height="315" src="http://www.youtube.com/embed/4F2m91eKmts" frameborder="0" allowfullscreen></iframe>
+       //        `
+       //  $("#videos").append(video)
 }
